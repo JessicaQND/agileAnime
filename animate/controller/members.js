fa0b9cedf63f3f52734305600193558d78302a2c
Glenn bubbleboix <20639058@student.uwa.edu.au>
2017-05-23 21:47:54 +0800

    lol

--- animate/controller/members.js
+++ animate/controller/members.js
@@ -15,10 +15,17 @@ function index(req,res){
 	}
       );
 };
-
+//uploading the data base onto mlab
 module.exports.newMember = function(req,res){
-  var newmember = new member({firstname: req.body.firstname, lastname: req.body.lastname, age: req.body.age, username: req.body.username, password : req.body.password,
-  int1: req.body.int1, int2: req.body.int2, int3: req.body.int3});
+  var newmember = new member({firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    age: req.body.age,
+    username: req.body.username,
+    password : req.body.password,
+    int1: req.body.int1,
+    int2: req.body.int2,
+    int3: req.body.int3
+  });
   newmember.save(function(err, data){
     if(err){
       console.log(err);
@@ -30,7 +37,21 @@ module.exports.newMember = function(req,res){
     }
     else{
       console.log(data, ' saved');
-      index(req, res);
+      res.render('login');
     }
   });
 };
+//getting user name and password authencation
+module.exports.login = function(req,res){
+  var user = db.Memeber.find(req.body.username);
+  var password = user.password;
+  console.log('loggin')
+  res.render('/')
+
+}
+//returning a view of information to the user profile page
+
+//edit/edit information off a user profile page
+//the algorithm for producing a list index of matches
+//start the chat
+//log out
