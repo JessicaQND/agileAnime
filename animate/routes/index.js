c3c66bf39dfb7888aadf52407b47862a92d758ed
JessicaQND <21123195@student.uwa.edu.au>
2017-05-23 19:59:42 +0800

    added static sites for register, login, home and respective route in index.js

--- animate/routes/index.js
+++ animate/routes/index.js
@@ -3,6 +3,17 @@ var Memberdb = require('../model/members')
 var router = express.Router();
 var ctrlMemeber = require('../controller/members');
 
+router.get('/', function(req, res) {
+    res.render('index');
+});
+
+router.get('/register', function(req, res) {
+    res.render('register', {});
+});
+
+router.get('/login', function(req, res) {
+    res.render('login', {});
+});
 
 router.post('/signup', ctrlMemeber.newMember);
 
